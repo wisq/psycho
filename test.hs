a2dfc3bf255c9dc8599dc9b0cd5496e9c96fdf1a
Adrian Irving-Beer <wisq@wisq.net>
2013-02-28 18:21:49 -0500

    Working parsing.

--- test.hs
+++ test.hs
@@ -3,10 +3,29 @@
 import qualified Database.Redis as Redis
 import Control.Monad.IO.Class (liftIO)
 import Data.ByteString (ByteString, concat)
-import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS
 import Data.Either
 
 import Text.JSON
+
+
+data Worker = Worker {
+	job_class :: String
+} deriving (Show)
+
+instance JSON Worker where
+	readJSON json = do
+		obj <- readJSON json
+		payload <- obj ! "payload"
+		job_class <- payload ! "class"
+		return Worker {job_class = job_class}
+	    where
+		(!) :: JSON a => JSObject JSValue -> String -> Result a
+		(!) = flip valFromObj
+
+	showJSON = undefined
+
+
 
 main = do
 	conn <- Redis.connect Redis.defaultConnectInfo
@@ -14,27 +33,18 @@ main = do
 		worker_ids   <- Redis.smembers "resque:workers"
 		worker_jsons <- sequence $ getWorkers worker_ids
 		let workers = parseWorkers worker_jsons
-		liftIO $ print $ head workers
+		liftIO $ print workers
  where
 	getWorkers (Right ids)  = map getWorker ids
 	getWorker id = Redis.get $ BS.concat ["resque:worker:", id]
 
 parseWorkers = foldl (++) [] . map parse
  where
-	parse (Right (Just json)) = [parseWorker(json)]
+	parse (Right (Just json)) = [parseWorker json]
 	parse (Right Nothing) = []
 
-parseWorker json = json
-
-data Worker = Worker {
-	job_class :: String
-} deriving (Show)
-
-makeWorker :: JSObject JSValue -> Result Worker
-makeWorker json = do
-	payload <- json ! "payload"
-	job_class <- payload ! "class"
-	return Worker {job_class = job_class}
+parseWorker :: ByteString -> Worker
+parseWorker = fromOk . decode . BS.unpack
  where
-	(!) :: JSON a => JSObject JSValue -> String -> Result a
-	(!) = flip valFromObj
+	fromOk (Ok x) = x
+	fromOk (Error e) = error ("JSON parsing failed: " ++ e)
