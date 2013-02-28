48413194a2c68de4237e9be926b7a5268711b5da
Adrian Irving-Beer <wisq@wisq.net>
2013-02-28 18:02:56 -0500

    Namespace Redis.

--- test.hs
+++ test.hs
@@ -1,21 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Database.Redis
+import qualified Database.Redis as Redis
 import Control.Monad.IO.Class (liftIO)
 import Data.ByteString (ByteString, concat)
 import qualified Data.ByteString as BS
 import Data.Either
 
 main = do
-	conn <- connect defaultConnectInfo
-	runRedis conn $ do
-		worker_ids   <- smembers "resque:workers"
+	conn <- Redis.connect Redis.defaultConnectInfo
+	Redis.runRedis conn $ do
+		worker_ids   <- Redis.smembers "resque:workers"
 		worker_jsons <- sequence $ getWorkers worker_ids
 		let workers = parseWorkers worker_jsons
 		liftIO $ print $ head workers
  where
 	getWorkers (Right ids)  = map getWorker ids
-	getWorker id = get $ BS.concat ["resque:worker:", id]
+	getWorker id = Redis.get $ BS.concat ["resque:worker:", id]
 
 parseWorkers = foldl (++) [] . map parse
  where
