a4952a03e0834870217a7036065b96f83320bb81
Adrian Irving-Beer <wisq@wisq.net>
2013-03-01 00:02:54 -0500

    Add --all and cleanup options parsing.

--- main.hs
+++ main.hs
@@ -11,37 +11,42 @@ data Flag = RedisHost String
 	  | RedisPort Int
 
 data Options = Options
-  { optRedisInfo :: Redis.ConnectInfo
+  { optRedisInfo :: Redis.ConnectInfo,
+    optAllHosts  :: Bool
   }
 
 defaultOptions :: Options
 defaultOptions = Options
   { optRedisInfo = Redis.defaultConnectInfo
+  , optAllHosts  = False
   }
 
 options :: [ OptDescr (Options -> IO Options) ]
 options =
-    [ Option "h" ["host"]
-	(ReqArg
-	    (\arg opt -> return opt { optRedisInfo = (optRedisInfo opt) { Redis.connectHost = arg } })
-	    "HOST")
-	"Redis server host"
+	[ Option "h" ["host"]
+		(ReqArg (\arg opt -> return $ setRedisHost opt arg)
+		"HOST")
+		"Redis server host"
 
-    , Option "p" ["port"]
-	(ReqArg
-	    (\arg opt -> return opt { optRedisInfo = (optRedisInfo opt) { Redis.connectPort = redisPort arg } })
-	    "PORT")
-	"Redis server port"
+	, Option "p" ["port"]
+		(ReqArg (\arg opt -> return $ setRedisPort opt arg)
+		"PORT")
+		"Redis server port"
 
-    , Option [] ["help"]
-	(NoArg
-	    (\_ -> do
+	, Option "a" ["all"]
+		(NoArg (\opt -> return opt { optAllHosts = True }))
+		"Select workers on all hosts"
+
+	, Option [] ["help"]
+		(NoArg (\_ -> do
 		prg <- getProgName
 		hPutStrLn stderr (usageInfo prg options)
 		exitWith ExitSuccess))
-	"Show help"
-    ]
+		"Show help"
+	]
  where
+	setRedisHost opt arg = opt { optRedisInfo = (optRedisInfo opt) { Redis.connectHost = arg } }
+	setRedisPort opt arg = opt { optRedisInfo = (optRedisInfo opt) { Redis.connectPort = redisPort arg } }
 	redisPort = Redis.PortNumber . fromIntegral . read
 
 
@@ -50,5 +55,5 @@ main = do
 	let (actions, nonOptions, errors) = getOpt RequireOrder options args
 	opts <- foldl (>>=) (return defaultOptions) actions
 
-	workers <- myWorkers $ optRedisInfo opts
+	workers <- (if optAllHosts opts then allWorkers else myWorkers) $ optRedisInfo opts
 	print workers
